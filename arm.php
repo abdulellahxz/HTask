4a24f7cb3f3bfe4a8458a478ccc2f79267d62579
abdulellahxz <abdulellahxz@gmail.com>
2021-07-12 20:21:45 +0300

    Initial commit

---
+++ arm.php
@@ -0,0 +1,42 @@
+<?php
+$dbServerName = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "task";
+$conn = mysqli_connect( $dbServerName, $dbUsername, $dbPassword, $dbName );
+?>
+  
+   <!DOCTYPE html>
+   <html lang="en">
+   <head>
+       <meta charset="UTF-8">
+       <title>Data trasfering</title>
+   </head>
+   <body>
+    <?php
+    $E1=$_POST["Engine1"];
+    $E2=$_POST["Engine2"];
+    $E3=$_POST["Engine3"];
+    $E4=$_POST["Engine4"];
+    $E5=$_POST["Engine5"];
+    $E6=$_POST["Engine6"];
+    if ( in_array( "Save", $_POST ) ) {
+    $state = 0;
+    } else if ( in_array("Play",$_POST ) ) {
+    $state = 1;
+    }
+    $query ="insert into arm(arm1,arm2,arm3,arm4,arm5,arm6,state)values({$E1},{$E2},{$E3},{$E4},{$E5},{$E6},{$state})";
+    $result = mysqli_query( $conn, $query );
+    if ( $result ) {
+        echo "Data added sucessfully";
+    } else {
+        die( "Data could not added to the database".mysqli_connect_error() );
+    }
+?>
+   </body>
+   </html>
+<?php
+mysqli_close( $conn );
+?>
+
+
